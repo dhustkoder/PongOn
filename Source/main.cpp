1d989d374036fda9900da4e41c53fa72cd348d67
Rafael Moura <rafaelmoura.dev@gmail.com>
2016-09-09 17:14:53 -0300

    nick support

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,33 +1,39 @@
 #include <cmath>
+#include <cassert>
 #include <cstring>
 #include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
 constexpr const unsigned int WIN_WIDTH {512};
 constexpr const unsigned int WIN_HEIGHT {256};
-constexpr const unsigned short CONNECTION_PORT {7171};
+
 
 struct Position {
 	float top, bottom, left, right;
 };
 
 struct Ball : sf::CircleShape {
-	static constexpr const float RADIUS {10.5};
-	static constexpr const float VELOCITY {7.5f};
+	static constexpr const float RADIUS {10.5f};
+	static constexpr const float VELOCITY {2.5f};
 	Ball() : sf::CircleShape(RADIUS) {
 		setPosition(WIN_WIDTH / 2, WIN_HEIGHT / 2);
 		setOrigin(RADIUS, RADIUS);
+		setFillColor(sf::Color::Green);
+		setOutlineColor(sf::Color::Magenta);
 	}
 };
 
 struct Paddle : sf::RectangleShape {
-	static constexpr const float WIDTH {15};
-	static constexpr const float HEIGHT {60};
+	static constexpr const float WIDTH {15.f};
+	static constexpr const float HEIGHT {60.f};
 	static constexpr const float VELOCITY {8.8f};
 	Paddle(sf::Vector2f pos) : sf::RectangleShape({WIDTH, HEIGHT}) {
 		setPosition(pos);
 		setOrigin(WIDTH / 2, HEIGHT / 2);
+		setFillColor(sf::Color::Red);
+		setOutlineColor(sf::Color::Green);
 	}
 };
 
@@ -39,8 +45,8 @@ struct Shapes {
 
 struct Velocities {
 	sf::Vector2f ball {Ball::VELOCITY, Ball::VELOCITY / 4};
-	float player {0};
-	float cpu {0};
+	float player {0.f};
+	float cpu {0.f};
 };
 
 struct Positions {
@@ -49,26 +55,43 @@ struct Positions {
 	Position cpu;
 };
 
-static sf::TcpSocket tcp_socket;
-static bool is_server = false;
+struct Connection {
+	static constexpr const unsigned short CONNECTION_PORT {7171};
+	static sf::TcpSocket socket;
+	static std::string local_nick;
+	static std::string remote_nick;
+	static std::size_t bytes_received;
+	static bool is_server;
+
+	static bool BootAsServer();
+	static bool BootAsClient();
+	static void AskNicks();
+	static bool SwitchNicks();
+	static void SendVel(float vel);
+	static void ReceiveVel(float* vel);
+};
+
+sf::TcpSocket Connection::socket;
+std::string Connection::local_nick;
+std::string Connection::remote_nick;
+std::size_t Connection::bytes_received{0};
+bool Connection::is_server{false};
 
 
-static bool boot_as_server();
-static bool boot_as_client();
 static void update_positions(const Shapes& shapes, Positions* positions);
 static void update_velocities(const Positions& positions, Velocities* velocities);
 static void update_shapes(const Velocities& velocities, Shapes* shapes);
-static void process_input(Velocities* velocities);
+static void process_input(sf::Keyboard::Key code, bool pressed, Velocities* velocities);
 
 
 int main(int argc, char** argv)
 {
 	if (argc > 1) {
 		if (std::strcmp(argv[1], "-server") == 0) {
-			if(!boot_as_server())
+			if(!Connection::BootAsServer())
 				return EXIT_FAILURE;
 		} else if (std::strcmp(argv[1], "-client") == 0) {
-			if(!boot_as_client())
+			if(!Connection::BootAsClient())
 				return EXIT_FAILURE;
 		}
 	} else {
@@ -88,9 +111,11 @@ int main(int argc, char** argv)
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
 			switch (event.type) {
-			case sf::Event::KeyPressed: /*fall*/
+			case sf::Event::KeyPressed:
+				process_input(event.key.code, true, &velocities);
+				break;
 			case sf::Event::KeyReleased:
-				process_input(&velocities);
+				process_input(event.key.code, false, &velocities);
 				break;
 			case sf::Event::Closed:
 				window.close();
@@ -104,7 +129,7 @@ int main(int argc, char** argv)
 		update_velocities(positions, &velocities);
 		update_shapes(velocities, &shapes);
 		
-		window.clear(sf::Color::Red);
+		window.clear(sf::Color::Blue);
 		window.draw(shapes.ball);
 		window.draw(shapes.player);
 		window.draw(shapes.cpu);
@@ -171,30 +196,18 @@ void update_velocities(const Positions& positions, Velocities* const velocities)
 			vel = 0;
 	};
 	
-	const auto send_vel = [](float vel) {
-		if (tcp_socket.send(&vel, sizeof(vel)) != sf::Socket::Done)
-		    std::cerr << "failed to send data!\n";
-	};
-	const auto receive_vel = [](float& vel) {
-		std::size_t dummy;
-		if (tcp_socket.receive(&vel, sizeof(vel), dummy) != sf::Socket::Done)
-		    std::cerr << "failed to receive data!\n";
-	};
-	
-	if (is_server) {
+	if (Connection::is_server) {
 		const auto& local_pos = positions.player;
 		auto& local_vel = velocities->player;
-		auto& client_vel = velocities->cpu;
 		check_vel(local_pos, local_vel);
-		send_vel(local_vel);
-		receive_vel(client_vel);
+		Connection::SendVel(local_vel);
+		Connection::ReceiveVel(&velocities->cpu);
 	} else {
 		const auto& local_pos = positions.cpu;
 		auto& local_vel = velocities->cpu;
-		auto& server_vel = velocities->player;
 		check_vel(local_pos, local_vel);
-		receive_vel(server_vel);
-		send_vel(local_vel);
+		Connection::ReceiveVel(&velocities->player);
+		Connection::SendVel(local_vel);
 	}
 }
 
@@ -216,54 +229,121 @@ void update_shapes(const Velocities& velocities, Shapes* const shapes)
 }
 
 
-void process_input(Velocities* const velocities)
+void process_input(const sf::Keyboard::Key code, const bool pressed, Velocities* const velocities)
 {
-	float& vel = is_server ? velocities->player : velocities->cpu;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-	    vel = -Paddle::VELOCITY;
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		vel = Paddle::VELOCITY;
-	else
+	float& vel = Connection::is_server ? velocities->player : velocities->cpu;
+	if (pressed) {
+		switch (code) {
+		case sf::Keyboard::W: vel = -Paddle::VELOCITY; break;
+		case sf::Keyboard::S: vel = Paddle::VELOCITY; break;
+		default: vel = 0; break;
+		}
+	} else {
 		vel = 0;
+	}
 }
 
 
 
-bool boot_as_server()
+bool Connection::BootAsServer()
 {
 	sf::TcpListener listener;
 	is_server = true;
-	
+	AskNicks();
+
 	std::cout << "booting as server...\n";
-	
-	if (listener.listen(CONNECTION_PORT) == sf::Socket::Done) {
-		std::cout << "waiting for client...\n"; 
-		listener.accept(tcp_socket);
-		std::cout << "client connected from: " << tcp_socket.getRemoteAddress() << '\n';
-	} else {
+	if (listener.listen(CONNECTION_PORT) != sf::Socket::Done) {
 		std::cerr << "failed to listen port " << CONNECTION_PORT << '\n';
 		return false;
 	}
 	
-	return true;
+	std::cout << "waiting for client...\n"; 
+	if (listener.accept(socket) != sf::Socket::Done) {
+		std::cerr << "connection failed\n";
+		return false;
+	}
+
+	return SwitchNicks();
 }
 
-bool boot_as_client()
+bool Connection::BootAsClient()
 {
-	sf::IpAddress ip;
+	sf::IpAddress serverIp;
+	AskNicks();
+
 	std::cout << "booting as client...\n";
-	std::cout << "enter the server ip address: ";
-	std::cin >> ip;
+	std::cout << "enter the server\'s ip address: ";
+	std::cin >> serverIp;
 	
-	if (tcp_socket.connect(ip, CONNECTION_PORT) == sf::Socket::Done) {
-		std::cout << "connetion succed!\n";
-	} else {
+	if (socket.connect(serverIp, CONNECTION_PORT) != sf::Socket::Done) {
 		std::cerr << "connection failed!\n";
 		return false;
 	}
-	
+
+	return SwitchNicks();
+}
+
+void Connection::AskNicks()
+{
+	std::cout << "enter your nickname: ";
+	std::cin >> Connection::local_nick;
+}
+
+
+bool Connection::SwitchNicks()
+{
+	const auto send = [] {
+		const auto local_nick_size = local_nick.size();
+		if (socket.send(&local_nick_size, sizeof(local_nick_size)) != sf::Socket::Done) {
+			std::cerr << "failed to send local nick size\n";
+			return false;
+		}
+		const auto nbytes = sizeof(local_nick[0]) * local_nick_size;
+		if (socket.send(&local_nick[0], nbytes) != sf::Socket::Done) {
+			std::cerr << "failed to send local nick\n";
+			return false;
+		}
+
+		return true;
+	};
+	const auto receive = [] {
+		std::size_t dummy;
+		std::string::size_type remote_nick_size;
+		if (socket.receive(&remote_nick_size, sizeof(remote_nick_size), dummy)) {
+			std::cerr << "failed to receive remote nick size\n";
+			return false;
+		}
+
+		remote_nick.resize(remote_nick_size);
+		const auto nbytes = sizeof(local_nick[0]) * remote_nick_size;
+		if (socket.receive(&remote_nick[0], nbytes, dummy) != sf::Socket::Done) {
+			std::cerr << "failed to receive remote nick\n";
+			return false;
+		}
+
+		return true;
+	};
+
+	if (is_server) {
+		if (!send() || !receive())
+			return false;
+	} else {
+		if (!receive() || !send())
+			return false;
+	}
+
+	std::cout << "connected to: " << remote_nick << '\n';
 	return true;
 }
 
 
+void Connection::SendVel(const float vel) {
+	if (socket.send(&vel, sizeof(vel)) != sf::Socket::Done)
+		std::cerr << "failed to send data!\n";
+}
 
+
+void Connection::ReceiveVel(float* const vel) {
+	if (socket.receive(vel, sizeof(*vel), bytes_received) != sf::Socket::Done)
+		std::cerr << "failed to receive data!\n";
+}
